f8dd1a9e2898c410a0c1ee52c92f47639f2a88b9
nachool <nachma.olivia1394@gmail.com>
2025-10-25 23:53:26 +0700

    menambah coloum & gambar

--- app/src/main/java/com/example/basickotlin/activity1.kt
+++ app/src/main/java/com/example/basickotlin/activity1.kt
@@ -1,2 +1,42 @@
 package com.example.basickotlin
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun album(modifier: Modifier){
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.Black)
+            .padding(all = 20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        val gambar = painterResource(id = R.drawable.ellipse_47)
+        Image(
+            painter = gambar,
+            contentDescription = null,
+            modifier = Modifier
+                .size(120.dp)
+                .clip(CircleShape)
+        )
+
+        Spacer(modifier = Modifier.height(height = 24.dp))
+
+
+    }
+}
