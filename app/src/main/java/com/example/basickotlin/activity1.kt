0f4b648a99074b2a0b8964c1686e84c22499c7e8
nachool <nachma.olivia1394@gmail.com>
2025-10-26 00:52:37 +0700

    nambah row twrakhir

--- app/src/main/java/com/example/basickotlin/activity1.kt
+++ app/src/main/java/com/example/basickotlin/activity1.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -76,8 +77,10 @@ fun album(modifier: Modifier){
                     .size(120.dp)
                     .border(
                         width = 5.dp,
-                        color = colorResource(id = R.color.white)
+                        color = colorResource(id = R.color.white),
+                        shape = RoundedCornerShape(16.dp)
                     )
+                    .clip(RoundedCornerShape(16.dp))
             )
 
         }
@@ -97,8 +100,10 @@ fun album(modifier: Modifier){
                         .size(120.dp)
                         .border(
                             width = 5.dp,
-                            color = colorResource(id = R.color.white)
+                            color = colorResource(id = R.color.white),
+                            shape = RoundedCornerShape(16.dp)
                         )
+                        .clip(RoundedCornerShape(16.dp))
                 )
             }
 
@@ -111,13 +116,31 @@ fun album(modifier: Modifier){
                         .size(120.dp)
                         .border(
                             width = 5.dp,
-                            color = colorResource(id = R.color.white)
+                            color = colorResource(id = R.color.white),
+                            shape = RoundedCornerShape(16.dp)
                         )
+                        .clip(RoundedCornerShape(16.dp))
                 )
             }
 
 
 
+        }
+
+
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Box(
+
+            ){
+                Image(painter = painterResource(id = R.drawable.dangerouswoman), contentDescription = null,
+                modifier = Modifier
+                    .size(120.dp))
+            }
         }
 
     }
