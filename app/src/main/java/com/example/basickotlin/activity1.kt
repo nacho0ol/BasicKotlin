a5b03d2c9e3cb1ff09430ea205eb3f3d0ceae5f3
nachool <nachma.olivia1394@gmail.com>
2025-10-26 00:42:22 +0700

    nambah text

--- app/src/main/java/com/example/basickotlin/activity1.kt
+++ app/src/main/java/com/example/basickotlin/activity1.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -23,7 +24,10 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun album(modifier: Modifier){
@@ -48,7 +52,16 @@ fun album(modifier: Modifier){
                 )
         )
 
-        Spacer(modifier = Modifier.height(height = 24.dp))
+        Spacer(modifier = Modifier.height(height = 30.dp))
+
+        Text(
+            text = stringResource(id = R.string.teks1),
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold,
+            color = colorResource(id = R.color.white)
+        )
+
+        Spacer(modifier = Modifier.height(height = 30.dp))
 
         Box(
             modifier = Modifier.fillMaxWidth(),
